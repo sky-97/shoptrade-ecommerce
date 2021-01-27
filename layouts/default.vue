60e535c92a38513fc2d26a05062c03471bee6464
sky-97 <akashshetty022.as@gmail.com>
2021-01-27 19:19:18 +0530

    samll changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,12 +5,10 @@
   </div>
 </template>
 <script>
-import Navbar from '~/components/Navbar'
-import Footer from '~/components/Footer'
+import Navbar from '~/components/Navbar.vue'
 export default {
   components : {
     Navbar,
-    Footer
   }
 }
 </script>
