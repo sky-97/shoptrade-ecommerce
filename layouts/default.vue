592bfe8b2342a606a3698acc762983d55bfdfd5a
sky-97 <akashshetty022.as@gmail.com>
2021-01-27 17:19:40 +0530

    commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,8 +6,8 @@
   </div>
 </template>
 <script>
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import Navbar from '~/components/Navbar'
+import Footer from '~/components/Footer'
 export default {
   components : {
     Navbar,
