018384cc399d9c85a3a6cc34c3a7239637e0d86d
sky-97 <akashshetty022.as@gmail.com>
2021-01-27 21:57:41 +0530

    wrong name

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <Navbar />
+    <!-- <Navbar /> -->
     <Nuxt />
   </div>
 </template>
 <script>
-import Navbar from '../components/Navbar.vue'
+// import Navbar from '../components/Navbar.vue'
 export default {
   components : {
-    Navbar,
+    // Navbar,
   }
 }
 </script>
