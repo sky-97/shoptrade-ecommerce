d335d0ff0fedc4ec4eb1fcdb81218df0e557135e
sky-97 <akashshetty022.as@gmail.com>
2021-01-27 20:42:17 +0530

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <!-- <Navbar /> -->
+    <Navbar />
     <Nuxt />
   </div>
 </template>
 <script>
-// import Navbar from '../components/Navbar.vue'
+import Navbar from './../components/Navbar.vue'
 export default {
   components : {
     Navbar,
